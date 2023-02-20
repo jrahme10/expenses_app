8198035d70e64293dcef7f0c24f3c05ea1a2d727
jrahme10 <jrahme10@gmail.com>
2023-02-20 21:07:50 +0200

    second commit

--- lib/screens/hompage.dart
+++ lib/screens/hompage.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import '../styles/styles.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 30, 15, 15),
+                child: Text(
+                  'Save your money with expense tracker',
+                  style: homeText,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 20),
+                child: Text(
+                  'Save money! the more your money works for you, the less you have to work for money',
+                  style: homeTextBottom,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Let\'s Start'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/styles/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//main colors
+Color firstColor = Colors.black;
+Color secondColor = Colors.purple;
+Color fistColor = Colors.black;
+
+//homescreen
+TextStyle homeText = const TextStyle(
+  color: Colors.black,
+  fontSize: 30,
+  fontFamily: 'opensans_bold',
+);
+
+TextStyle homeTextBottom = const TextStyle(
+  color: Color.fromARGB(100, 0, 0, 0),
+  fontSize: 20,
+  fontFamily: 'opensans',
+);
